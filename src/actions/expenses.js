e062bc8853bd6f2ca8bfccc02108c9779afca82e
Kamil <kamil@kamil.com>
2018-09-08 17:35:33 +0200

    Edit expense

--- src/actions/expenses.js
+++ src/actions/expenses.js
@@ -25,17 +25,25 @@ export const startAddExpense = (expenseData = {}) => {
     };
 };
 
-// REMOVE_EXPENSE
-export const removeExpense = ({ id } = {}) => ({
-    type: 'REMOVE_EXPENSE',
-    id
-});
-
 // EDIT_EXPENSE
 export const editExpense = (id, updates) => ({
     type: 'EDIT_EXPENSE',
     id,
     updates
+});
+
+export const startEditExpense = (id, updates) => {
+    return (dispatch) => {
+        return database.ref(`expenses/${id}`).update(updates).then(() => {
+            dispatch(editExpense(id, updates));
+        });
+    };
+};
+
+// REMOVE_EXPENSE
+export const removeExpense = ({ id } = {}) => ({
+    type: 'REMOVE_EXPENSE',
+    id
 });
 
 export const startRemoveExpense = ({id} = {}) => {
