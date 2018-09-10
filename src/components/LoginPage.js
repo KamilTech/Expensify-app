2ba0bd43153a20804b5e82b4ad3a193258a67df9
Kamil <kamil@kamil.com>
2018-09-10 21:40:34 +0200

    style login, prefix

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -6,8 +6,8 @@ export const LoginPage = ({ startLogin }) => (
     <div className="box-layout">
         <div className="box-layout__box">
             <h1 className="box-layout__title">Expensify App</h1>
-            <p>Some description</p>
-            <button className="button" onClick={startLogin}>Login</button>
+            <p>Save your expenses in one place</p>
+            <button className="button" onClick={startLogin}>Login with google</button>
         </div>
     </div>
 );
